1d8f61a878fb963811a70077ffeac5e609d60d00
raff24 <raflitetsuya234@gmail.com>
2022-03-17 18:53:10 +0800

    Tugas python Oop

---
+++ YiskaAstuti_D0219401_A.py
@@ -0,0 +1,11 @@
+class Laptop:
+    def __init__(self, inputNama, inputHarga, inputWarna):
+        self.nama = inputNama
+        self.harga = inputHarga
+        self.warna = inputWarna
+
+Laptop1 = Laptop("Hp",5000000,"Hitam")
+Laptop2 = Laptop("asus",8000000,"Silver")
+
+print(Laptop2.harga)
+    
